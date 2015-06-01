885343eb635450c42a5bbe4b6c9a3e56123a5b44
Petter Hareim <petter.hareim@bouvet.no>
2015-06-01 15:23:23 +0200

    added login

--- petters-todo.js
+++ petters-todo.js
@@ -7,17 +7,45 @@ if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
         tasks: function() {
-            return Tasks.find({}, {
-                sort: {
-                    createdAt: -1
-                }
-            });
+            if (Session.get("hideCompleted")) {
+                // If hide completed is checked, filter tasks
+                return Tasks.find({
+                    checked: {
+                        $ne: true
+                    }
+                }, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
+            } else {
+                // Otherwise, return all of the tasks
+                return Tasks.find({}, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
+            }
+        },
+        hideCompleted: function() {
+            return Session.get("hideCompleted");
         },
         authors: function() {
             return Authors.find({});
+        },
+        incompleteCount: function() {
+            return Tasks.find({
+                checked: {
+                    $ne: true
+                }
+            }).count();
         }
     });
+
     Template.body.events({
+        "change .hide-completed input": function(event) {
+            Session.set("hideCompleted", event.target.checked);
+        },
         "submit .new-task": function(event) {
             // This function is called when the new task form is submitted
 
@@ -25,7 +53,9 @@ if (Meteor.isClient) {
 
             Tasks.insert({
                 text: text,
-                createdAt: new Date() // current time
+                createdAt: new Date(), // current time
+                owner: Meteor.userId(), // _id of logged in user
+                username: Meteor.user().username // username of logged in user
             });
 
             // Clear form
@@ -47,6 +77,10 @@ if (Meteor.isClient) {
         "click .delete": function() {
             Tasks.remove(this._id);
         }
+    });
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
     });
 };
 
